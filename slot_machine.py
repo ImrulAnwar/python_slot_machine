94748318cef0928fb7402a2500198f8a61988a61
Imrul Anwar <imrulanwar836@gmail.com>
2023-01-16 21:04:08 +0600

    added the deposit function

--- slot_machine.py
+++ slot_machine.py
@@ -1,1 +1,14 @@
-print("hello")
+def deposit():
+    while (True):
+        amount = input("How much do you want to deposit? ")
+        if (amount.isdigit()):
+            amount = int(amount)
+            if (amount > 0):
+                break
+            else:
+                print("Please enter a positive number")
+        else:
+            print("Please enter a valid number")
+    return amount
+
+deposit()
